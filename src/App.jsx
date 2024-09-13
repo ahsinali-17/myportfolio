d3bc5002e3e5300cc4ba48db8e32fbe9e0c35818
ahsinali-17 <aajoker17@gmail.com>
2024-09-13 19:53:46 +0500

    navigation changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+import { Element } from 'react-scroll'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import About from './components/About'
@@ -7,32 +7,27 @@ import Services from './components/Services'
 import Project from './components/Project'
 import Contact from './components/Contact'
 import './App.css'
+
 const App = () => {
-  let router = createBrowserRouter([
-    {
-      path: '/',
-      element: <div className='relative'><Navbar/><><Home/><About/><Services/><Project/><Contact/></></div>
-    },
-    {
-      path: '/about',
-      element: <><Navbar/><About/></>
-    },
-    {
-      path: '/services',
-      element: <><Navbar/><Services/></>
-    },
-    {
-      path: '/projects',
-      element: <><Navbar/><Project/></>
-    },
-    {
-      path: '/contact',
-      element: <><Navbar/><Contact/></>
-    }
-  ])
   return (
-    <div>
-      <RouterProvider router={router}/>
+    <div className='relative overflow-x-hidden'>
+      <Navbar />
+      
+      <Element name="home">
+        <Home />
+      </Element>
+      <Element name="about">
+        <About />
+      </Element>
+      <Element name="services">
+        <Services />
+      </Element>
+      <Element name="projects">
+        <Project />
+      </Element>
+      <Element name="contact">
+        <Contact />
+      </Element>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,43 +1,120 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import './Navbar.css';
+import React from "react";
+import { Link } from "react-scroll";
+import "./Navbar.css";
 
 const Navbar = () => {
   const handleClick = () => {
-    const Links = document.querySelector('.right');
-    if (Links.style.display === 'block') {
-        Links.style.display = 'none';
+    const Links = document.querySelector(".right");
+    if (Links.style.display === "block") {
+      Links.style.display = "none";
     } else {
-        Links.style.display = 'block';
+      Links.style.display = "block";
     }
   };
 
   const handleShow = () => {
-    const Links = document.querySelector('.right');
-    Links.style.display = 'none';
-  }
+    const Links = document.querySelector(".right");
+    if(Links.style.display === "block") 
+    Links.style.display = "none";
+  };
 
   return (
-      <header className="m-0 text-white h-[10vh] fixed top-0 w-full">
-        <nav className="flex px-12 justify-between items-center bg-violet-800 h-full w-screen">
-        <div className="logo text-2xl cursor-pointer w-1/3"><Link className="logopic hover:text-red-600" to='/'>Portfolio<span className='dot text-4xl text-red-600'>.</span></Link></div>
-        
-          <div className="burger" onClick={handleClick}>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#ffffff" fill="none">
-              <path d="M4 5L20 5" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-              <path d="M4 12L20 12" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-              <path d="M4 19L20 19" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-            </svg>
-          </div>
+    <header className="m-0 text-white h-[10vh] fixed top-0 w-full overflow-hidden">
+      <nav className="flex px-12 justify-between items-center bg-violet-800 h-full w-screen">
+        <div className="logo text-2xl cursor-pointer w-1/3">
+          <Link
+          onClick={handleShow}
+            className="logopic hover:text-red-600"
+            to="home"
+            smooth={true}
+            duration={500}
+            offset={-70}
+          >
+            Portfolio<span className="dot text-4xl text-red-600">.</span>
+          </Link>
+        </div>
 
-          <div className="right">
-              <div><Link to='/about' className='hover:text-red-600 cursor-pointer'>About</Link></div>
-              <div><Link to='/services' className='hover:text-red-600 cursor-pointer'>Services</Link></div>
-              <div><Link to='/projects' className='hover:text-red-600 cursor-pointer'>Projects</Link></div>
-              <div><Link to='/contact' className='hover:text-red-600 cursor-pointer'>Contact</Link></div>
+        <div className="burger" onClick={handleClick}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+            color="#ffffff"
+            fill="none"
+          >
+            <path
+              d="M4 5L20 5"
+              stroke="currentColor"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              d="M4 12L20 12"
+              stroke="currentColor"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              d="M4 19L20 19"
+              stroke="currentColor"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </div>
+
+        <div className="right">
+          <div>
+            <Link
+            onClick={handleShow}
+              to="about"
+              smooth={true}
+              duration={500}
+              className="hover:text-red-600 cursor-pointer"
+            >
+              About
+            </Link>
           </div>
-        </nav>
-      </header>
+          <div>
+            <Link
+            onClick={handleShow}
+              to="services"
+              smooth={true}
+              duration={500}
+              className="hover:text-red-600 cursor-pointer"
+            >
+              Services
+            </Link>
+          </div>
+          <div>
+            <Link
+            onClick={handleShow}
+              to="projects"
+              smooth={true}
+              duration={500}
+              className="hover:text-red-600 cursor-pointer"
+            >
+              Projects
+            </Link>
+          </div>
+          <div>
+            <Link
+            onClick={handleShow}
+              to="contact"
+              smooth={true}
+              duration={500}
+              className="hover:text-red-600 cursor-pointer"
+            >
+              Contact
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </header>
   );
 };
 
