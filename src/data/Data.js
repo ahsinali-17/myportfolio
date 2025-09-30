614b4f9f32d93399d2ff523a77582dc06be870c8
ahsinali-17 <aajoker17@gmail.com>
2025-09-30 16:51:04 +0500

    ai chat bot

--- src/data/Data.js
+++ src/data/Data.js
@@ -10,7 +10,7 @@ export const Bio = {
     postalCode: "47040",
     province: "Punjab",
     timezone: "GMT+5",
-    address: "<iframe src=\"https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3315.8508719120705!2d72.716082!3d33.790349!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzPCsDQ3JzI1LjMiTiA3MsKwNDInNTcuOSJF!5e0!3m2!1sen!2s!4v1759232196734!5m2!1sen!2s\" width=\"600\" height=\"450\" style=\"border:0;\" allowfullscreen=\"\" loading=\"lazy\" referrerpolicy=\"no-referrer-when-downgrade\"></iframe>",
+    // address: "<iframe src=\"https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3315.8508719120705!2d72.716082!3d33.790349!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzPCsDQ3JzI1LjMiTiA3MsKwNDInNTcuOSJF!5e0!3m2!1sen!2s!4v1759232196734!5m2!1sen!2s\" width=\"600\" height=\"450\" style=\"border:0;\" allowfullscreen=\"\" loading=\"lazy\" referrerpolicy=\"no-referrer-when-downgrade\"></iframe>",
   },
   email: "aahsinali17@gmail.com",
   phone: +923126627792,
